3d839e55a740674c6826f448f319a2e137be048c
Hojun Cha <hojun.cha997@gmail.com>
2024-03-14 21:46:47 +0900

    세션 관련 함수

---
+++ src/auth/utils.js
@@ -0,0 +1,76 @@
+// // routes
+// import { PATH_AUTH } from '../routes/paths';
+// // utils
+// import { axios } from '../utils/axios';
+
+// // ----------------------------------------------------------------------
+
+// function jwtDecode(token) {
+//   const base64Url = token.split('.')[1];
+//   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+//   const jsonPayload = decodeURIComponent(
+//     window
+//       .atob(base64)
+//       .split('')
+//       .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
+//       .join('')
+//   );
+
+//   return JSON.parse(jsonPayload);
+// }
+
+// // ----------------------------------------------------------------------
+
+// export const isValidToken = (accessToken) => {
+//   if (!accessToken) {
+//     return false;
+//   }
+
+//   const decoded = jwtDecode(accessToken);
+
+//   const currentTime = Date.now() / 1000;
+
+//   return decoded.exp > currentTime;
+// };
+
+// // ----------------------------------------------------------------------
+
+// export const tokenExpired = (exp) => {
+//   // eslint-disable-next-line prefer-const
+//   let expiredTimer;
+
+//   const currentTime = Date.now();
+
+//   // Test token expires after 10s
+//   // const timeLeft = currentTime + 10000 - currentTime; // ~10s
+//   const timeLeft = exp * 1000 - currentTime;
+
+//   clearTimeout(expiredTimer);
+
+//   expiredTimer = setTimeout(() => {
+//     alert('세션이 만료되었습니다.');
+
+//     localStorage.removeItem('accessToken');
+
+//     window.location.href = PATH_AUTH.login;
+//   }, timeLeft);
+// };
+
+// // ----------------------------------------------------------------------
+
+// export const setSession = (accessToken) => {
+//   if (accessToken) {
+//     localStorage.setItem('accessToken', accessToken);
+
+//     axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+
+//     // This function below will handle when token is expired
+//     const { exp, user_nm } = jwtDecode(accessToken); // ~3 days by minimals server
+//     tokenExpired(exp);
+//     console.log(jwtDecode(accessToken));
+//     return user_nm;
+//   }
+//   localStorage.removeItem('accessToken');
+//   delete axios.defaults.headers.common.Authorization;
+//   return null;
+// };
