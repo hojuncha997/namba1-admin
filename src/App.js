47da923c8da381d5d595eed5b867089f436d1d9e
hojuncha997 <hojun.cha997@gmail.com>
2024-03-17 17:10:37 +0900

    App에 AuthProvider로 감싸기(전역 컨텍스트 공유 위해) 그리고 BrowserRouter이동

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 // 전역 스타일
 import "./App.css";
-
-import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
-
 // userRoutes를 사용한 커스텀 설정 라우터
+import { AuthProvider } from "./auth/JwtContext";
 import Router from "./routes";
-
-import logoImage from "./assets/images/namba-logo-100.png";
 import GlobalStyle from "./components/global/GlobalStyle";
-import UpperNav from "./components/upperNav/UpperNav";
-import MainContainer from "./components/mainContainer/MainContainer";
-import MainContent from "./components/MainContent/MainContent";
-import Sidebar from "./components/sidebar/Sidebar";
-
-import PersonalCompact from "./components/userMgmt/personalCompact/PersnoalCompact";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const navigate = useNavigate();
-
   return (
-    <>
-      <GlobalStyle />
-      <div className="App">
-        <Router />
-        {/* <UpperNav title={"UpperNav"}></UpperNav>
-        <Sidebar menus={"menus 안녕하세요!"}></Sidebar>
-
-        <MainContainer><Router /></MainContainer> */}
-      </div>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <GlobalStyle />
+        <div className="App">
+          <Router />
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
