889c05a2ec5359376fae5cba7f8db47ea5be0d95
hojuncha997 <hojun.cha997@gmail.com>
2024-03-17 18:04:06 +0900

    App.js 불필요 부분 제거

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 // 전역 스타일
 import "./App.css";
 // userRoutes를 사용한 커스텀 설정 라우터
+
 import { AuthProvider } from "./auth/JwtContext";
-import Router from "./routes";
-import GlobalStyle from "./components/global/GlobalStyle";
 import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./components/global/GlobalStyle";
+import Router from "./routes";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <GlobalStyle />
-        <div className="App">
-          <Router />
-        </div>
+        <Router />
       </BrowserRouter>
     </AuthProvider>
   );
